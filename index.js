77c3c5e0052508e702dc13bc279332f0a3bb8e57
Rahmot15 <mdrahmatulla666@gmail.com>
2025-07-23 00:17:58 +0600

    Mongodb setup and products get

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -9,6 +10,37 @@ app.use(cors())
 app.use(express.json())
 
 
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const db = client.db("Bazariodb")
+        const productsCollections = db.collection("products")
+
+        app.get('/products', async (req, res) => {
+            const result = await productsCollections.find().toArray()
+            res.send(result)
+        })
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
