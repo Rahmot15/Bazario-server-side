b76faf01d8864d08577865a20c9b612987b1ae05
Rahmot15 <mdrahmatulla666@gmail.com>
2025-07-24 07:01:23 +0600

    get a user's role

--- index.js
+++ index.js
@@ -128,6 +128,14 @@ async function run() {
             res.send(result)
         })
 
+        // get a user's role
+        app.get('/user/role/:email', async (req, res) => {
+            const email = req.params.email
+            const result = await usersCollection.findOne({ email })
+            if (!result) return res.status(404).send({ message: 'User Not Found.' })
+            res.send({ role: result?.role })
+        })
+
 
         // Post a review
         app.post("/reviews", async (req, res) => {
